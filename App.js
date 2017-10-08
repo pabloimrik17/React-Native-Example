c6a3ad86665e997867902da57ce0bb5e8099e4be
Etherless <pabloimrik17@gmail.com>
2017-10-08 23:29:49 +0200

    First demo components from react native docs

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Bananas from "./components/bananas";
+import LotsOfGreetings from "./components/greetings";
 
-export default class App extends React.Component {
+export default class HelloWorldApp extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+          <Text>Hello World!</Text>
+          <Bananas></Bananas>
+          <LotsOfGreetings></LotsOfGreetings>
       </View>
     );
   }
